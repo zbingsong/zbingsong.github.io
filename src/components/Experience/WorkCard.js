24291c764debf7539082c29888a3fe031912aef9
Bingsong <bingsong@usc.edu>
2024-01-08 15:04:15 -0800

    changed style of Work card, still need to make it clickable

--- src/components/Experience/WorkCard.js
+++ src/components/Experience/WorkCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import no_image from "../../Assets/Projects/no_image.jpg";
 
 function WorkCard(props) {
@@ -7,10 +8,23 @@ function WorkCard(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath ?? no_image} className="card-img-top" alt="card-img" />
       <Card.Body>
-        <a href={props.link ?? "#"} target="_blank" rel="noopener noreferrer">
-          <Card.Title>{props.title ?? " "}</Card.Title>
-          <Card.Text style={{ textAlign: "center" }}>{props.location ?? " "}</Card.Text>
-        </a>
+        <Card.Title>
+          <a href={props.link ?? "#"} 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            {props.title ?? " "}
+          </a>
+        </Card.Title>
+
+        <Card.Text style={{ textAlign: "center" }}>
+          {props.location ?? " "}
+        </Card.Text>
+        
+        <Button variant="primary" href={props.link} target="_blank">
+          Company site
+        </Button>
       </Card.Body>
     </Card>
   );
