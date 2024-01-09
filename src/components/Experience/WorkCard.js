cbdd805190927c1af4dd5ddbfaf95f7da84e0172
Bingsong <bingsong@usc.edu>
2024-01-08 17:47:45 -0800

    removed anchor of company name, has company site button already

--- src/components/Experience/WorkCard.js
+++ src/components/Experience/WorkCard.js
@@ -9,19 +9,13 @@ function WorkCard(props) {
       <Card.Img variant="top" src={props.imgPath ?? no_image} className="card-img-top" alt="card-img" />
       <Card.Body>
         <Card.Title>
-          <a href={props.link ?? "#"} 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            {props.title ?? " "}
-          </a>
+          {props.title ?? " "}
         </Card.Title>
 
         <Card.Text style={{ textAlign: "center" }}>
           {props.location ?? " "}
         </Card.Text>
-        
+
         <Button variant="primary" href={props.link} target="_blank">
           Company site
         </Button>
